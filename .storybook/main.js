8b0db37f164184cbb7eb2d114fb30503970ea441
Henrik Hofmeister <henrik@newdawn.dk>
2022-10-02 16:16:54 +0200

    Hande sizing better and added storybook

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+    stories: [
+        '../stories/**/*.stories.tsx'
+    ],
+    core: {
+        builder: 'webpack5'
+    }
+}
---
+++ stories/frontend-block-editor.stories.tsx
@@ -0,0 +1,103 @@
+import React, {useState} from 'react';
+import {BlockMetadata, BlockServiceSpec, BlockType, SchemaKind, TargetConfig} from '@blockware/ui-web-types';
+import {BlockTargetProvider} from '@blockware/ui-web-context';
+import FrontendBlockEditorComponent from '../src/web/FrontendBlockEditorComponent';
+
+import '@blockware/ui-web-components/styles/index.less';
+
+const BLOCK_KIND = 'blocks.blockware.com/v1/Frontend';
+
+const targetConfig: TargetConfig = {
+    kind: 'my-language-target',
+    name: 'My Language Target',
+    blockKinds: [
+        BLOCK_KIND
+    ]
+};
+
+const ServiceBlock: SchemaKind<BlockServiceSpec, BlockMetadata> = {
+    kind: BLOCK_KIND,
+    metadata: {
+        name: 'My block',
+        version: '1.2.3'
+    },
+    spec: {
+        type: 'frontend' as BlockType,
+        target: {
+            kind: targetConfig.kind
+        },
+        entities: {
+            source: {
+                type: 'blockware-dsl',
+                value: ''
+            },
+            types: [
+                {
+                    name: 'MyEntity',
+                    properties: {
+                        'id': {
+                            type: 'string'
+                        },
+                        'tags': {
+                            type: 'array',
+                            items: {
+                                type: 'string'
+                            }
+                        },
+                        'children': {
+                            type: 'array',
+                            items: {
+                                type: 'object',
+                                properties: {
+                                    childId: {
+                                        type: 'integer'
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            ]
+        }
+    }
+};
+
+BlockTargetProvider.register(targetConfig);
+
+export default {
+    title: 'Frontend Block'
+};
+
+export const CreateEditor = () => {
+
+    const [definition, setDefinition] = useState({kind:BLOCK_KIND,metadata:{name:'', version:''},spec:{type:BlockType.SERVICE,target:{kind:''}}});
+
+    return (
+        <FrontendBlockEditorComponent {...definition}
+                                     creating={true}
+                                     onDataChanged={((metadata, spec) => {
+                                         setDefinition({
+                                             kind: ServiceBlock.kind,
+                                             metadata,
+                                             spec
+                                         })
+                                     })}/>
+    )
+};
+
+export const EditEditor = () => {
+
+    const [definition, setDefinition] = useState(ServiceBlock);
+
+    return (
+        <FrontendBlockEditorComponent {...definition}
+                                     creating={false}
+                                     onDataChanged={((metadata, spec) => {
+            setDefinition({
+                kind: ServiceBlock.kind,
+                metadata,
+                spec
+            })
+        })}/>
+    )
+};
