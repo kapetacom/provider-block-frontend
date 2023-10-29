8b13b37d5bc6f744fb7e8aec6385365e5cfef371
Henrik Hofmeister <henrik@newdawn.dk>
2023-10-29 17:30:38 +0100

    chore: Add licensing (#13)

--- src/web/index.ts
+++ src/web/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2023 Kapeta Inc.
+ * SPDX-License-Identifier: MIT
+ */
+
 import { IBlockTypeProvider } from "@kapeta/ui-web-types";
 import { FrontendBlockEditorComponent } from "./FrontendBlockEditorComponent";
 import FrontendBlockValidation from "./FrontendBlockValidation";
